a937b095cf04580964c7c373e9a09bbcdae280d6
LuisMcfly <luis.payares.ruiz@gmail.com>
2023-02-02 17:34:50 -0500

    Nested Routes - Guitars

--- app/routes/guitarras/index.jsx
+++ app/routes/guitarras/index.jsx
@@ -1,22 +1,12 @@
 import { useLoaderData } from '@remix-run/react'
 import { getGuitarras } from "~/models/guitarras.server"
 import ListadoGuitarras from '~/components/listado-guitarras'
-import styles from '~/styles/guitarras.css'
 
 export function meta() {
   return {
     title: 'GuitarLA - Tienda de Guitarras',
     description: 'GuitarLA - Nuestra colección de guitarras'
   }
-}
-
-export function links() {
-  return [
-    {
-      rel: 'stylesheet',
-      href: styles
-    }
-  ]
 }
 
 export async function loader() {
@@ -30,11 +20,9 @@ function Tienda() {
 
 
   return (
-    <main className='contenedor'>
       <ListadoGuitarras
         guitarras={guitarras}
       />
-    </main>
   )
 }
 
