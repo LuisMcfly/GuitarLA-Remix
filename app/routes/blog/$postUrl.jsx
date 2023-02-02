31851b8f6b61db38b3aec3e0a7ee671e14e6e964
LuisMcfly <luis.payares.ruiz@gmail.com>
2023-02-02 18:17:57 -0500

    Nested Routes - Blog

--- app/routes/blog/$postUrl.jsx
+++ app/routes/blog/$postUrl.jsx
@@ -1,7 +1,6 @@
 import { useLoaderData } from '@remix-run/react'
 import { getPost } from '~/models/posts.server'
 import { formatearFecha } from "~/utils/helpers"
-import styles from '~/styles/blog.css'
 
 export async function loader({ params }) {
     const { postUrl } = params
@@ -31,22 +30,13 @@ export function meta({ data }) {
     }
 }
 
-export function links() {
-    return [
-        {
-            rel: 'stylesheet',
-            href: styles
-        }
-    ]
-}
-
 function Post() {
 
     const post = useLoaderData();
     const { titulo, contenido, imagen, publishedAt } = post.data[0].attributes
 
     return (
-        <article className='contenedor post mt-3'>
+        <article className='post mt-3'>
             <img className='imagen' src={imagen.data.attributes.url} alt={`Imagen del post ${titulo}`} />
 
             <div className="contenido">
